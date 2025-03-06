c1c0baa40a075474ec808b645e5dd66005b8c979
Mikaela-D <G00401392@atu.ie>
2025-03-06 22:04:26 +0000

    Formatted code

--- routes/shop.js
+++ routes/shop.js
@@ -1,61 +1,66 @@
 // C:\Users\Mikaela\Cloud Computing\Week7JsonMongoose\Week7JsonMongoose\routes\shop.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   name: { type: String, required: true },
   price: { type: String, required: true },
   ourId: { type: String, required: true },
   anArray: { type: Array, required: false },
-  anObject: { type: Object, required: false }
-})
+  anObject: { type: Object, required: false },
+});
 
-const Product = mongoose.model('product', productSchema) // 'product' refers to the collection, so maps products collection to productSchema; see lecture notes
+const Product = mongoose.model("product", productSchema); // 'product' refers to the collection, so maps products collection to productSchema; see lecture notes
 
-let nextProductId = 0
-router.get('/addProduct', (req, res, next) => {
-  new Product({ ourId: '9' + nextProductId, name: 'widget', price: 3.95, size: 'large' })
+let nextProductId = 0;
+router.get("/addProduct", (req, res, next) => {
+  new Product({
+    ourId: "9" + nextProductId,
+    name: "widget",
+    price: 3.95,
+    size: "large",
+  })
     .save()
-    .then(result => {
-      nextProductId++
-      console.log('saved product to database')
-      res.redirect('/')
+    .then((result) => {
+      nextProductId++;
+      console.log("saved product to database");
+      res.redirect("/");
     })
-    .catch(err => {
-      console.log('failed to addAproduct: ' + err)
-      res.redirect('/')
-    })
-})
+    .catch((err) => {
+      console.log("failed to addAproduct: " + err);
+      res.redirect("/");
+    });
+});
 
-router.get('/', (req, res, next) => {
-  console.log(req.query)
+router.get("/", (req, res, next) => {
+  console.log(req.query);
   Product.find() // Always returns an array
-    .then(products => {
-      res.send(JSON.stringify(products))
+    .then((products) => {
+      res.send(JSON.stringify(products));
     })
-    .catch(err => {
-      console.log('Failed to find: ' + err)
-      res.send(JSON.stringify(err))
-    })
-})
+    .catch((err) => {
+      console.log("Failed to find: " + err);
+      res.send(JSON.stringify(err));
+    });
+});
 
-router.post('/', (req, res, next) => {
-  console.log(req.body)
+router.post("/", (req, res, next) => {
+  console.log(req.body);
   Product.find() // Always returns an array
-    .then(products => {
-      res.json({ success: true, Products: products })
+    .then((products) => {
+      res.json({ success: true, Products: products });
     })
-    .catch(err => {
-      console.log('Failed to find: ' + err)
-      res.json({ success: false, theError: err })
-    })
-})
+    .catch((err) => {
+      console.log("Failed to find: " + err);
+      res.json({ success: false, theError: err });
+    });
+});
 
-router.get('/getSpecificProduct', async (req, res) => {
+router.get("/getSpecificProduct", async (req, res) => {
   try {
     const { ourId } = req.query;
     if (!ourId) {
@@ -75,7 +80,7 @@ router.get('/getSpecificProduct', async (req, res) => {
   }
 });
 
-router.post('/getSpecificProduct', async (req, res) => {
+router.post("/getSpecificProduct", async (req, res) => {
   try {
     const { ourId } = req.body;
     if (!ourId) {
@@ -95,30 +100,29 @@ router.post('/getSpecificProduct', async (req, res) => {
   }
 });
 
+router.get("/updateSpecificProduct", (req, res, next) => {
+  Product.find({ ourId: "1" }) // Always returns an array
+    .then((products) => {
+      let specificProduct = products[0]; // pick the first match
+      specificProduct.price = 199.95;
+      specificProduct.save(); // Should check for errors here too
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log("Failed to find product: " + err);
+      res.send("No product found");
+    });
+});
 
-router.get('/updateSpecificProduct', (req, res, next) => {
-  Product.find({ ourId: '1' }) // Always returns an array
-    .then(products => {
-      let specificProduct = products[0] // pick the first match
-      specificProduct.price = 199.95
-      specificProduct.save() // Should check for errors here too
-      res.redirect('/')
+router.get("/deleteSpecificProduct", (req, res, next) => {
+  Product.findOneAndRemove({ ourId: "0" })
+    .then((resp) => {
+      res.redirect("/");
     })
-    .catch(err => {
-      console.log('Failed to find product: ' + err)
-      res.send('No product found')
-    })
-})
+    .catch((err) => {
+      console.log("Failed to find product: " + err);
+      res.send("No product found");
+    });
+});
 
-router.get('/deleteSpecificProduct', (req, res, next) => {
-  Product.findOneAndRemove({ ourId: '0' })
-    .then(resp => {
-      res.redirect('/')
-    })
-    .catch(err => {
-      console.log('Failed to find product: ' + err)
-      res.send('No product found')
-    })
-})
-
-exports.routes = router
+exports.routes = router;
